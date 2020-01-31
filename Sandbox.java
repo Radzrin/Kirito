216f2e0e3bcd16919dd744a9e2b5dcd2154e0f5d
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-01-31 14:11:24 -0500

    Add files via upload

--- Sandbox.java
+++ Sandbox.java
@@ -6,8 +6,18 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Prints "Press alt F4" to the terminal window.
-        System.out.println("\nHello, World"
-        	+ "all you had to do was follow the damn train");
+        System.out.println("\nHello, World");
         	
-    }
+
+        	int a, b;
+        	double c;
+
+        	a = 5;
+        	b = 2;
+        	c = ((double)a)/b;
+
+        	System.out.println("the result of the division is" + c); 
+
+        	
+    }  
 }
